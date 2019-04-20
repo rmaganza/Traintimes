9b656914e909050c967de39dcdc933550685b947
Riccardo Maganza <riccardo.maganza@xtream>
2019-04-20 22:46:45 +0200

    Add Kafka Consumer

--- streamprocessing/kafkaconf.py
+++ streamprocessing/kafkaconf.py
@@ -4,14 +4,15 @@ from kafka import KafkaProducer, KafkaConsumer
 
 KAFKA_PORT = '6667'
 BASE_SERVER = 'sandbox-hdp.hortonworks.com:'
-
+GROUP_NAME = 'traingroup'
 Producer = KafkaProducer(bootstrap_servers=BASE_SERVER + KAFKA_PORT)
 Consumer = KafkaConsumer(bootstrap_servers=BASE_SERVER + KAFKA_PORT,
                          auto_offset_reset='earliest',
                          consumer_timeout_ms=5 * 60 * 1000,
                          enable_auto_commit=True,
                          auto_commit_interval_ms=45000,
-                         value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+                         value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+                         group_id=GROUP_NAME
                          )
 
 TOPIC_NAME = 'trains'
